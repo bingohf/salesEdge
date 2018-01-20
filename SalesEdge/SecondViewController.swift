64b080fa45392db200e663b60ef9d1a1721a6c95
Bin Guo <binguo@bindemac-mini.local>
2018-01-20 19:34:41 +0800

    Initial Commit

---
+++ SalesEdge/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SalesEdge
+//
+//  Created by Bin Guo on 2018/1/20.
+//  Copyright © 2018年 Bin Guo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
