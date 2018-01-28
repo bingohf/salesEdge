8a2810b5e36759b80007796e4c5fb98f11a7a385
Bin Guo <togb@qq.com>
2018-01-28 16:38:30 +0800

    add keyborad return

--- SalesEdge/SampleController.swift
+++ SalesEdge/SampleController.swift
@@ -11,9 +11,14 @@ import XLPagerTabStrip
 import QRCodeReader
 import AVFoundation
 
-class SampleController: UIViewController,QRCodeReaderViewControllerDelegate {
+class SampleController: UIViewController,QRCodeReaderViewControllerDelegate,UITextFieldDelegate {
 
+    @IBOutlet weak var mFieldBillNo: UITextField!
+
+    @IBOutlet weak var mWebView: UIWebView!
+    @IBOutlet weak var mFieldPANO: UITextField!
     
+    @IBOutlet weak var mIndicator: UIActivityIndicatorView!
     lazy var readerVC: QRCodeReaderViewController = {
         let builder = QRCodeReaderViewControllerBuilder {
             $0.reader = QRCodeReader(metadataObjectTypes: [.qr,.code39,.code128, .upce,.aztec,.code93,.dataMatrix,.ean13,.pdf417], captureDevicePosition: .back)
@@ -24,6 +29,8 @@ class SampleController: UIViewController,QRCodeReaderViewControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mWebView.loadHTMLString("Hello world", baseURL: nil)
+        mIndicator.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,30 +38,40 @@ class SampleController: UIViewController,QRCodeReaderViewControllerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func onPAQRCodeClick(_ sender: Any) {
+        scanQRCode(){qrcodeResult in
+            self.mFieldPANO.text = qrcodeResult?.value
+        }
+    }
+    
+    @IBAction func onBillQrCodeClick(_ sender: Any) {
+        scanQRCode(){qrcodeResult in
+            self.mFieldBillNo.text = qrcodeResult?.value
+        }
+    }
+    
     @IBAction func onMoreMenuTap(_ sender: Any) {
         let alertController = UIAlertController(title:"alert", message:"Select action",preferredStyle:UIAlertControllerStyle.actionSheet)
         let groupQRCodeAction = UIAlertAction(title:"Change Group", style:.default){
             (action: UIAlertAction!) -> Void in
-            self.scanARcode()
+           // self.scanARcode()
         }
         alertController.addAction(groupQRCodeAction)
         alertController.popoverPresentationController?.barButtonItem = sender as? UIBarButtonItem
         self.present(alertController, animated:true,completion:nil)
     }
     
-    func scanARcode() {
+    func scanQRCode(callback: @escaping (QRCodeReaderResult?) -> Void){
         readerVC.delegate = self
         
         // Or by using the closure pattern
-        readerVC.completionBlock = { (result: QRCodeReaderResult?) in
-            print(result)
-        }
+        readerVC.completionBlock = callback
         
         // Presents the readerVC as modal form sheet
         readerVC.modalPresentationStyle = .formSheet
         present(readerVC, animated: true, completion: nil)
     }
+    
     func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
         reader.stopScanning()
         
@@ -68,6 +85,13 @@ class SampleController: UIViewController,QRCodeReaderViewControllerDelegate {
         reader.stopScanning()
         
         dismiss(animated: true, completion: nil)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == mFieldPANO || textField == mFieldBillNo{
+            textField.resignFirstResponder()
+            return true
+        }
+        return false
     }
 }
 
