8e7f5b5662b230415e9a4fb4512a02122a90159d
Bin Guo <togb@qq.com>
2018-03-25 14:24:01 +0800

    fix bug

--- SalesEdge/PAViewController.swift
+++ SalesEdge/PAViewController.swift
@@ -12,14 +12,16 @@ import WebKit
 class PAViewController: UIViewController,UIWebViewDelegate {
 
     @IBOutlet weak var mWebView: UIWebView!
-    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        let myURL = URL(string: "http://ledwayazure.cloudapp.net/mobile")
-        let myRequest = URLRequest(url: myURL!)
-        mWebView.loadRequest(myRequest)
+        if let pdaGuid = getPdaGuid().addingPercentEncoding(withAllowedCharacters: .urlHostAllowed){
+            let myURL = URL(string: "http://ledwayazure.cloudapp.net/mobile?pdaGuid=\(pdaGuid)")
+            let myRequest = URLRequest(url: myURL!)
+            mWebView.loadRequest(myRequest)
+        }
+     
         mWebView.delegate = self;
-        self.view.makeToast("This is a piece of toast")
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +29,19 @@ class PAViewController: UIViewController,UIWebViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func getPdaGuid() -> String! {
+        let deviceId = UIDevice.current.identifierForVendor!.uuidString;
+        let deviceName = UIDevice.current.modelName
+        let dformatter = DateFormatter()
+        dformatter.dateFormat = "yyyyMMdd'T'HHmmss.S"
+        let timeStamp = dformatter.string(from: Date.init())
+        var language = Locale.preferredLanguages.first!
+        var languageArr = language.components(separatedBy: "-")
+        while languageArr.count > 2 {
+            languageArr.remove(at: 1)
+        }
+        language = languageArr.joined(separator: "_")
+        return "\(deviceId)-\(deviceName)-LEDWAY-\(timeStamp)~\(language)"
+    }
 }
 
